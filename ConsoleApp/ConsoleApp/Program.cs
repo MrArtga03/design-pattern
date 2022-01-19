b77903e24acc5b1d584494e8c5f5ca58550f625e
MrArtga <artga03@gmail.com>
2022-01-19 20:11:46 +0300

    LR01

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            Duck redheadDuck = new RedheadDuck();
+
+            Duck[] ducks = new Duck[] { mallardDuck, redheadDuck };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Display());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Quack());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
